cb0cc59a3ef9674f6287e35ef422bd0d735d818c
Tyler Martin <tyler.martin@nist.gov>
2023-02-14 09:32:06 -0500

    feat: overhaul of AcquisitionFunction for performance

--- AFL/agent/AcquisitionFunction.py
+++ AFL/agent/AcquisitionFunction.py
@@ -29,14 +29,9 @@ class Acquisition:
         self.composition_atol = 0.015#absolute distance tolerace
         self.metric_rtol = 0.03 #pct tolerance
     
-    def reset_phasemap(self,phasemap,components):
+    def reset_phasemap(self,phasemap):
         self.phasemap = phasemap
-        self.components = components
-        self.grid_components = [k+"_grid" for k in components]
         
-    def reset_mask(self,mask):
-        self.mask = mask
-
     def add_exclusion(self,points):
         pass
     
@@ -48,8 +43,8 @@ class Acquisition:
         else:
             pm = self.phasemap.copy()
             
-        pm.afl.comp.plot_continuous(components=self.grid_components,labels='acq_metric')
-        pm.afl.comp.plot_discrete(components=self.components,set_labels=False)
+        pm.afl.comp.plot_continuous(components=self.phasemap.attrs['components_grid'],labels='acq_metric')
+        pm.afl.comp.plot_discrete(components=self.phasemap.attrs['components'],set_labels=False)
         
         if self.next_sample is not None:
             plt.plot(*PhaseMap.to_xy(np.array([self.next_sample.squeeze().values])).T,marker='x',color='r')
@@ -68,74 +63,72 @@ class Acquisition:
         else:
             sample_randomly = False
 
-        if self.mask is None:
+        if 'mask' not in self.phasemap:
             mask = slice(None)
         else:
-            mask = self.mask
+            mask = self.phasemap.mask
             
-        print("Creating ordered metric lists....(this is more expensive than it sounds)")
+        print("Creating ordered metric lists...")
         if sample_randomly:
-            all_metric = self.phasemap.where(mask,drop=True)
-            metric_grid = all_metric.afl.comp.get_grid(self.components)
-            
-            close_metric_idex_list=np.arange(all_metric.sizes['grid'])#these are shuffled below
-            far_metric_idex_list = []
+            close_metric = (
+                self.phasemap.sel(grid=mask,drop=False)
+                [composition.attrs['components_grid']] #just take composition values
+                .rename({k:k.replace('_grid','') for k in composition.attrs['components_grid']})
+                .to_array('component')
+            )
         else:
-            all_metric = self.phasemap.where(mask,drop=True).copy()
-            metric_grid = all_metric.afl.comp.get_grid(self.components)
-            
-            #sometimes all_metric has a 'grid' index...and sometimes it doesn't
-            try:
-                metric = all_metric.reset_index('grid').reset_coords(self.phasemap.attrs['components_grid']).copy()
-            except KeyError:
-                metric = all_metric.copy()
-                
-            metric = metric.assign_coords(grid=np.arange(metric.sizes['grid']))
+            metric = self.phasemap.sel(grid=mask,drop=True)#.copy()
+            metric = metric[['acq_metric']+metric.attrs['components_grid']]
             metric = metric.sortby('acq_metric')
             
             #find samples within rtol of maximum metric value
-            max_val= metric['acq_metric'].max()
-            is_close = np.isclose(metric['acq_metric'],max_val,rtol=self.metric_rtol,atol=0)
-            metric_mask = metric['acq_metric'].copy(data=is_close)
-            
-            close_metric = metric.where(metric_mask,drop=True)
-            close_metric_idex_list= list(close_metric.grid.values)
-            
-            far_metric = metric.where(~metric_mask,drop=True)
-            far_metric_idex_list= list(far_metric.grid.values)
+            is_close = np.isclose(
+                metric['acq_metric'],
+                metric['acq_metric'].max(),
+                rtol=self.metric_rtol,
+                atol=0
+            )
+             
+            close_metric = (
+                metric.sel(grid=is_close,drop=False)
+                [metric.attrs['components_grid']] #just take composition values
+                .rename({k:k.replace('_grid','') for k in metric.attrs['components_grid']})
+                .to_array('component')
+            )
 
+        close_metric_idex_list = list(close_metric.grid.values) #list of integers
         shuffle(close_metric_idex_list)
+        
         print(f"Running get_next_sample with sample_randomly={sample_randomly}")
         i = 0
         while True:
             print(f"Running iteration {i}")
             
             if close_metric_idex_list:
-                print(f"Getting \'close_metric'...")
+                print(f"Getting random point within {self.metric_rtol:0.2f} of maximum...")
                 idex = close_metric_idex_list.pop()
-            elif far_metric_idex_list:
-                print(f"Getting \'far_metric'...")
-                idex = far_metric_idex_list.pop()
             else:
                 raise ValueError('No gridpoint found that satisfies constraints! Try increasing composition_rtol!')
             
-            composition = metric_grid.isel(grid=idex).expand_dims('grid')
+            composition = close_metric.sel(grid=idex)
             
             if composition_check is None:
                 break #all done
-
-            dist = pairwise_distances(composition_check,composition).squeeze()[()]
+        
+            print(f"Verifying that gridpoint isn't on top of previous measurement...")
+            dist = (composition_check - composition).pipe(np.square).sum('component').pipe(np.sqrt)
+            #if (dist<self.composition_atol).any():
             if (dist<self.composition_atol).any():
                 i+=1
             else:
                 break
-
+        
             if nth>=self.phasemap.grid.shape[0]:
                 raise ValueError(f'No next sample found! Searched {nth} iterations!')
             elif nth>1000:
                 raise ValueError('Find next sample failed to converge!')
 
-        print(f"Found that gridpoint {idex} satisfyings the acquistion function and all constraints")
+        print(f"Found that gridpoint {idex} satisfies the acquistion function and all constraints")
         self.next_sample = composition
         return self.next_sample
 
@@ -148,7 +141,7 @@ class Variance(Acquisition):
         if self.phasemap is None:
             raise ValueError('No phase map set for acquisition! Call reset_phasemap!')
             
-        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.components).values)
+        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.phasemap.attrs['components']).values)
         self.phasemap['acq_metric'] = ('grid',self.y_var.sum(1))
         self.phasemap.attrs['acq_metric'] = self.name
 
@@ -163,7 +156,7 @@ class Random(Acquisition):
         if self.phasemap is None:
             raise ValueError('No phase map set for acquisition! Call reset_phasemap!')
             
-        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.components).values)
+        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.phasemap.attrs['components_grid']).values)
             
         indices = np.arange(self.phasemap['grid'].shape[0])
         random.shuffle(indices)
@@ -185,23 +178,11 @@ class IterationCombined(Acquisition):
         self.iteration = 1
         self.function2_frequency=function2_frequency
         
-    def reset_phasemap(self,phasemap,components):
-        self.function1.reset_phasemap(phasemap,components)
-        self.function2.reset_phasemap(phasemap,components)
-        self.phasemap = phasemap
-        self.components = components
-        self.grid_components = [k+"_grid" for k in components]
-        
-    def reset_mask(self,mask):
-        self.function1.reset_mask(mask)
-        self.function2.reset_mask(mask)
-        self.mask = mask
-        
     def calculate_metric(self,GP):
         if self.function1.phasemap is None:
             raise ValueError('No phase map set for acquisition! Call reset_phasemap!')
 
-        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.components).values)
+        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.phasemap.attrs['components_grid']).values)
         
         if ((self.iteration%self.function2_frequency)==0):
             print(f'Using acquisition function {self.function2.name} of iteration {self.iteration}')
@@ -231,7 +212,7 @@ class LowestDensity(Acquisition):
             raise ValueError('No phase map set for acquisition! Call reset_phasemap!')
             
         #this must be calculated regardless of whether it is used
-        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.components).values)
+        self.y_mean,self.y_var = GP.predict(self.phasemap.afl.comp.get_grid(self.phasemap.attrs['components_grid']).values)
             
         xy = self.phasemap.afl.comp.to_xy()
         xy_grid = self.phasemap.afl.comp.to_xy(self.phasemap.attrs['components_grid'])
