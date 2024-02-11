3d638ba132ec496fef842f9b306c8847679e31ab
Brad Sutliff <bpsutliff@gmail.com>
2024-02-11 17:46:35 -0500

    [feat] add hetero/homoscedactic uncertainty to the GPR in Extrapolator.py

--- AFL/double_agent/Extrapolator.py
+++ AFL/double_agent/Extrapolator.py
@@ -15,11 +15,11 @@ from AFL.double_agent.util import listify
 
 class Extrapolator(PipelineOp):
     def __init__(self, feature_input_variable, predictor_input_variable, output_variables, output_prefix,
-                 grid_variable, grid_dim, sample_dim, name='Extrapolator'):
+                 grid_variable, grid_dim, sample_dim, predictor_uncertainty_variable=None, name='Extrapolator'):
 
         super().__init__(
             name=name,
-            input_variable=[feature_input_variable, predictor_input_variable, grid_variable],
+            input_variable=[feature_input_variable, predictor_input_variable, grid_variable, predictor_uncertainty_variable],
             output_variable=[output_prefix+'_'+o for o in listify(output_variables)],
             output_prefix = output_prefix,
         )
@@ -76,28 +76,46 @@ class GaussianProcessClassifier(Extrapolator):
         return self
 
 class GaussianProcessRegressor(Extrapolator):
-    def __init__(self, feature_input_variable, predictor_input_variable, output_prefix, grid_variable, grid_dim,
-                 sample_dim, kernel=None, name='GaussianProcessRegressor'):
+    def __init__(self, feature_input_variable, predictor_input_variable,
+                 output_prefix, grid_variable, grid_dim,
+                 sample_dim, predictor_uncertainty_variable=None,
+                 kernel=None, name='GaussianProcessRegressor'):
 
         super().__init__(name=name, feature_input_variable=feature_input_variable,
                          predictor_input_variable=predictor_input_variable,
                          output_variables=['mean','std'], output_prefix=output_prefix,
+                         predictor_uncertainty_variable=predictor_uncertainty_variable,
                          grid_variable=grid_variable, grid_dim=grid_dim, sample_dim=sample_dim)
 
         if kernel is None:
-            self.kernel = sklearn.gaussian_process.kernels.Matern(length_scale=1.0, nu=1.5)
+            self.kernel = sklearn.gaussian_process.kernels.Matern(length_scale=[0.1,0.1],
+                                                                  # length_scale_bounds="fixed",
+                                                                  length_scale_bounds=[(1e-3,1e0),(1e-3,1e0)],
+                                                                  nu=2.5)
         else:
             self.kernel = kernel
+
+        self.predictor_uncertainty_variable = predictor_uncertainty_variable
+        self._banned_from_attrs.append('predictor_uncertainty_variable')
 
     def calculate(self, dataset):
         X = dataset[self.feature_input_variable].transpose(self.sample_dim, ...)
         y = dataset[self.predictor_input_variable].transpose(self.sample_dim, ...)
+
         grid = dataset[self.grid_variable]
 
-        clf = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel).fit(X.values, y.values)
-        mean, std = clf.predict(grid.values, return_std=True)
+        if self.predictor_uncertainty_variable != None:
+            dy = dataset[self.predictor_uncertainty_variable].transpose(self.sample_dim, ...)   
+            reg = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel, alpha=dy.values).fit(X.values, y.values)
+            reg_type = "homoscedastic"
+        else:
+            reg = sklearn.gaussian_process.GaussianProcessRegressor(kernel=self.kernel).fit(X.values, y.values)
+            reg_type = "heteroscedastic"
+           
+        mean, std = reg.predict(grid.values, return_std=True)
 
         self.output[self._prefix_output("mean")] = xr.DataArray(mean,dims=self.grid_dim)
         self.output[self._prefix_output("std")] = xr.DataArray(std,dims=self.grid_dim)
+        self.output[self._prefix_output("std")].attrs["Heteroscedastic"] = reg_type
 
         return self
