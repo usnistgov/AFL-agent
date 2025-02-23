d4d081a88f5861388d08e4484d45fcdf38e52285
Tyler Martin <tyler.martin@nist.gov>
2025-02-22 19:22:15 -0500

    [docs] adds correct path modification to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -125,7 +125,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/"))
+sys.path.insert(0, os.path.abspath("../../"))
+sys.path.append(os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
@@ -196,7 +197,3 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["source/_static"]
-
-import sys
-
-sys.path.append("../src/")
