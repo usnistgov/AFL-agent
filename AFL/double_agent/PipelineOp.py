e49747a37d07f04eea8ce30e9e6565900485502f
Tyler Martin <tyler.martin@nist.gov>
2025-03-08 00:03:47 -0500

    [docs] adds tutorial for using prefab pipelines

--- AFL/double_agent/PipelineOp.py
+++ AFL/double_agent/PipelineOp.py
@@ -30,20 +30,25 @@ class PipelineOp(ABC):
         Prefix for output variables when using pattern matching
     """
 
-    def __init__(self,
-                 name: Optional[str] | List[str] = None,
-                 input_variable: Optional[str] | List[str] = None,
-                 output_variable: Optional[str] | List[str] = None,
-                 input_prefix: Optional[str] | List[str] = None,
-                 output_prefix: Optional[str] | List[str] = None):
+    def __init__(
+        self,
+        name: Optional[str] | List[str] = None,
+        input_variable: Optional[str] | List[str] = None,
+        output_variable: Optional[str] | List[str] = None,
+        input_prefix: Optional[str] | List[str] = None,
+        output_prefix: Optional[str] | List[str] = None,
+    ):
 
-        if all(x is None for x in [input_variable, output_variable, input_prefix, output_prefix]):
+        if all(
+            x is None
+            for x in [input_variable, output_variable, input_prefix, output_prefix]
+        ):
             warnings.warn(
-                'No input/output information set for PipelineOp...this is likely an error',
-                stacklevel=2
+                "No input/output information set for PipelineOp...this is likely an error",
+                stacklevel=2,
             )
-        
-        self.name = name or 'PipelineOp'
+
+        self.name = name or "PipelineOp"
         self.input_variable = input_variable
         self.output_variable = output_variable
         self.input_prefix = input_prefix
@@ -51,7 +56,7 @@ class PipelineOp(ABC):
         self.output: Dict[str, xr.DataArray] = {}
 
         # variables to exclude when constructing attrs dict for xarray
-        self._banned_from_attrs = ['output', '_banned_from_attrs']
+        self._banned_from_attrs = ["output", "_banned_from_attrs"]
 
         ## PipelineContext
         try:
@@ -69,7 +74,10 @@ class PipelineOp(ABC):
         # Iterate through all frames in the stack.
         for frame_info in stack:
             # Look for __init__ functions where 'self' is our instance.
-            if frame_info.function == '__init__' and frame_info.frame.f_locals.get('self') is self:
+            if (
+                frame_info.function == "__init__"
+                and frame_info.frame.f_locals.get("self") is self
+            ):
                 valid_frames.append(frame_info)
         if valid_frames:
             # Choose the last __init__ call in the inheritance chain.
@@ -79,7 +87,7 @@ class PipelineOp(ABC):
             # Fallback: use the immediate caller if nothing was found.
             final_frame = inspect.currentframe().f_back
             args_info = inspect.getargvalues(final_frame)
-        
+
         # Build _stored_args by checking JSON serializability of each constructor argument.
         stored_args = {}
         for arg in args_info.args:
@@ -94,26 +102,30 @@ class PipelineOp(ABC):
                     )
                 stored_args[arg] = value
         self._stored_args = stored_args
-     
+
     def __getattribute__(self, name):
-        # Avoid recursion when accessing _stored_args.
-        if name == '_stored_args':
+        if name == "_stored_args":
+            try:
+                return object.__getattribute__(self, name)
+            except AttributeError:
+                return {}
+        try:
+            stored_args = object.__getattribute__(self, "_stored_args")
+        except AttributeError:
             return object.__getattribute__(self, name)
-        
-        stored_args = object.__getattribute__(self, "_stored_args")
         if name in stored_args:
             return stored_args[name]
         return object.__getattribute__(self, name)
-    
+
     def __setattr__(self, name, value):
         if name == "_stored_args":
             return object.__setattr__(self, name, value)
-        
+
         try:
             stored_args = object.__getattribute__(self, "_stored_args")
         except AttributeError:
             stored_args = None
-        
+
         if stored_args is not None and name in stored_args:
             try:
                 json.dumps(value)
@@ -125,7 +137,7 @@ class PipelineOp(ABC):
             stored_args[name] = value
         else:
             object.__setattr__(self, name, value)
-    
+
     def to_json(self):
         """
         Serializes the fully qualified class name and the constructor arguments
@@ -134,10 +146,7 @@ class PipelineOp(ABC):
         cls = self.__class__
         module = cls.__module__
         qualname = cls.__qualname__
-        data = {
-            "class": f"{module}.{qualname}",
-            "args": self._stored_args
-        }
+        data = {"class": f"{module}.{qualname}", "args": self._stored_args}
         return data
 
     @classmethod
@@ -159,7 +168,7 @@ class PipelineOp(ABC):
         pass
 
     def __repr__(self) -> str:
-        return f'<PipelineOp:{self.name}>'
+        return f"<PipelineOp:{self.name}>"
 
     def copy(self) -> Self:
         return copy.deepcopy(self)
@@ -167,7 +176,7 @@ class PipelineOp(ABC):
     def _prefix_output(self, variable_name: str) -> str:
         prefixed_variable = copy.deepcopy(variable_name)
         if self.output_prefix is not None:
-            prefixed_variable = f'{self.output_prefix}_{prefixed_variable}'
+            prefixed_variable = f"{self.output_prefix}_{prefixed_variable}"
         return prefixed_variable
 
     def _get_attrs(self) -> Dict:
@@ -178,7 +187,7 @@ class PipelineOp(ABC):
             except KeyError:
                 pass
 
-        #sanitize
+        # sanitize
         for key in output_dict.keys():
             output_dict[key] = str(output_dict[key])
             # if output_dict[key] is None:
@@ -190,16 +199,20 @@ class PipelineOp(ABC):
 
     def _get_variable(self, dataset: xr.Dataset) -> xr.DataArray:
         if self.input_variable is None and self.input_prefix is None:
-            raise ValueError((
-                """Can't get variable for {self.name} without input_variable """
-                """or input_prefix specified in constructor """
-            ))
+            raise ValueError(
+                (
+                    """Can't get variable for {self.name} without input_variable """
+                    """or input_prefix specified in constructor """
+                )
+            )
 
         if self.input_variable is not None and self.input_prefix is not None:
-            raise ValueError((
-                """Both input_variable and input_prefix were specified in constructor. """
-                """Only one should be specified to avoid ambiguous operation"""
-            ))
+            raise ValueError(
+                (
+                    """Both input_variable and input_prefix were specified in constructor. """
+                    """Only one should be specified to avoid ambiguous operation"""
+                )
+            )
 
         if self.input_variable is not None:
             output = dataset[self.input_variable].copy()
@@ -229,10 +242,12 @@ class PipelineOp(ABC):
                 value.attrs.update(self._get_attrs())
                 dataset1[name] = value
             else:
-                raise ValueError((
-                    f"""Items in output dictionary of PipelineOp {self.name} must be xr.Dataset or xr.DataArray """
-                    f"""Found variable named {name} of type {type(value)}."""
-                ))
+                raise ValueError(
+                    (
+                        f"""Items in output dictionary of PipelineOp {self.name} must be xr.Dataset or xr.DataArray """
+                        f"""Found variable named {name} of type {type(value)}."""
+                    )
+                )
         return dataset1
 
     def add_to_tiled(self, tiled_data):
@@ -242,20 +257,20 @@ class PipelineOp(ABC):
         # for name, dataarray in self.output.items():
         #     tiled_data.add_array(name, value.values)
 
-    def plot(self,**mpl_kwargs) -> plt.Figure:
+    def plot(self, **mpl_kwargs) -> plt.Figure:
         n = len(self.output)
-        if n>0:
-            fig, axes = plt.subplots(n,1,figsize=(8,n*4))
-            if n>1:
+        if n > 0:
+            fig, axes = plt.subplots(n, 1, figsize=(8, n * 4))
+            if n > 1:
                 axes = list(axes.flatten())
             else:
                 axes = [axes]
 
-            for i,(name,data) in enumerate(self.output.items()):
-                if 'sample' in data.dims:
-                    data = data.plot(hue='sample',ax=axes[i],**mpl_kwargs)
+            for i, (name, data) in enumerate(self.output.items()):
+                if "sample" in data.dims:
+                    data = data.plot(hue="sample", ax=axes[i], **mpl_kwargs)
                 else:
-                    data.plot(ax=axes[i],**mpl_kwargs)
+                    data.plot(ax=axes[i], **mpl_kwargs)
                 axes[i].set(title=name)
             return fig
         else:
